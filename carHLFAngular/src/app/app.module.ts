7bc2e30da1dbd66f776be5f0353159c44463ee59
mohamed zanon egypt <javazanon@gmail.com>
2022-01-08 16:00:08 +0300

    zanon add show history today

--- carHLFAngular/src/app/app.module.ts
+++ carHLFAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowHistoryComponent } from './diags/show-history/show-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowHistoryComponent
   ],
   imports: [
     BrowserModule,
